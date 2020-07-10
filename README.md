5695b3428a34fc09a31668e1ebb1bb9d8c18815b
renatomacedo1 <macedo.renato@protonmail.com>
2020-07-10 17:05:08 +0100

    Installed dependencies

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # blogapp
+
+Dependencies
+
+In the projects folder:
+
+npm init
+npm install --save express
+npm install --save express-handlebars
+npm install body-parser --save
+npm install --save mongoose
---
+++ app.js
@@ -0,0 +1,14 @@
+// Loading modules
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+//const mongoose = require("mongoose")
+
+// Configurations
+
+// Routes
+
+// Other
+
+const PORT = 8081;
+applicationCache.listen(PORT);
