3d893bd3ba90937450479248a23e35fbda6c71a6
renatomacedo1 <macedo.renato@protonmail.com>
2020-07-10 17:59:52 +0100

    Finished lesson 33

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # blogapp
 
-Dependencies
+In construction
 
-In the projects folder:
+Dependencies:
+
+Navigate to the projects folder and run the following commands:
 
 npm init
 npm install --save express
 npm install --save express-handlebars
 npm install body-parser --save
 npm install --save mongoose
+
+Now you have the dependencies installed
--- app.js
+++ app.js
@@ -2,13 +2,35 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
+const app = express();
+const admin = require("./routes/admin");
+const path = require("path");
 //const mongoose = require("mongoose")
 
-// Configurations
+// ########## Configurations ##########
+// ##### Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// ##### Handlebars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// ##### Mongoose
+// Em breve
+// ##### Public
+app.use(express.static(path.join(__dirname, "public"))); // Gets the absolute path to the directory "public"
+// ########## Routes ##########
+app.get("/", (req, res) => {
+  res.send("Rota principal");
+});
 
-// Routes
+app.get("/posts", (req, res) => {
+  res.send("Rota posts");
+});
 
+app.use("/admin", admin);
 // Other
 
 const PORT = 8081;
-applicationCache.listen(PORT);
+app.listen(PORT, () => {
+  console.log("Server is running!");
+});
